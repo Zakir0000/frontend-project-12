af9e462d98d3de20d344da518fda61808724afc7
Zakir0000 <zakirkhunkaev@gmail.com>
2024-12-16 08:34:44 +0300

    trouble with deploying

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,3 +1,27 @@
-const App = () => <h1>Hexlet Chat</h1>;
+import axios from 'axios';
+
+const App = async () => {
+  try {
+    await axios
+      .post('/api/v1/login', { username: 'admin', password: 'admin' })
+      .then((response) => {
+        console.log(response.data); // => { token: ..., username: 'admin' }
+      });
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTczNDMyNjIzNn0.dydVDNRnRsbV5oafSppSsBDsbWWMCov91_mnT-yDhlU';
+    const responseForGet = await axios
+      .get('/api/v1/channels', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data); // =>[{ id: '1', name: 'general', removable: false }, ...]
+      });
+    return <h1>Hexlet Chat</h1>;
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 export default App;
