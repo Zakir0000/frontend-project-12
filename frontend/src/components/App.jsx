b152f737344b3b523afa8a24309bfe373c6558c4
Zakir0000 <zakirkhunkaev@gmail.com>
2024-12-26 13:20:25 +0300

    check deploy issues

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,17 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import Rollbar from 'rollbar';
 import LoginPage from './LoginPage.jsx';
 import NotFound from './NotFound.jsx';
 import Chat from './Chat.jsx';
 import SignUpPage from './SignUpPage.jsx';
 import PrivateRoute from './PrivateRoute';
-import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
-import Rollbar from 'rollbar';
 
 const App = () => {
   const user = useSelector((state) => state.auth.user);
+
   const rollbarConfig = {
     accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
     captureUncaught: true,
@@ -26,28 +26,46 @@ const App = () => {
     code_version: '0.13.7',
     payload: {
       person: {
-        person: user ? { id: user.id, username: user.user } : null,
+        person: user ? user : null,
       },
     },
   };
   const rollbar = new Rollbar(rollbarConfig);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='*' element={<NotFound />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route
-          path='/'
-          element={
-            <PrivateRoute>
-              <Chat />
-            </PrivateRoute>
-          }
-        />
-        <Route path='/signup' element={<SignUpPage />} />
-      </Routes>
+    <RollbarProvider instance={rollbar}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<NotFound />} />
+          <Route
+            path='/login'
+            element={
+              <ErrorBoundary>
+                <LoginPage />
+              </ErrorBoundary>
+            }
+          />
+          <Route
+            path='/'
+            element={
+              <PrivateRoute>
+                <ErrorBoundary>
+                  <Chat />
+                </ErrorBoundary>
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path='/signup'
+            element={
+              <ErrorBoundary>
+                <SignUpPage />
+              </ErrorBoundary>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
       <ToastContainer position='top-right' autoClose={3000} />
-    </BrowserRouter>
+    </RollbarProvider>
   );
 };
 
