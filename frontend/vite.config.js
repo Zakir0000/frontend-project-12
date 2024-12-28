cc4c93586361da20fcdcca61fa77464496889509
Zakir0000 <zakirkhunkaev@gmail.com>
2024-12-28 16:12:34 +0300

    mod Makefile, package.json, vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5002,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
+        changeOrigin: true,
       },
       '/socket.io': {
-        target: 'ws://localhost:5001',
+        target: 'http://localhost:5001',
         ws: true,
         rewriteWsOrigin: true,
       },
