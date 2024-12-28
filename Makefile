8a2be79f479fa70ca8e2dd0f660caa8568009d3f
Zakir0000 <zakirkhunkaev@gmail.com>
2024-12-28 16:35:56 +0300

    mod makefile, package-lock, vite.config

--- Makefile
+++ Makefile
@@ -2,13 +2,16 @@ lint-frontend:
 	make -C frontend lint
 
 install:
-	npm install
+	npm ci
 
 start-frontend:
 	make -C frontend start
 
 start-backend:
 	npx start-server -s ./frontend/dist 
+
+deploy:
+	git push heroku main
 
 
 start:
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5002,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'http://localhost:5001',
+        target: 'ws://localhost:5001',
         ws: true,
         rewriteWsOrigin: true,
       },
