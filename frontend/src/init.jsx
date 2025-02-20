0a01d3aaaaf3a222416dae933716e2d9db41f466
Zakir0000 <zakirkhunkaev@gmail.com>
2025-02-20 10:06:50 +0300

    change rollbarConfig

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -3,7 +3,6 @@ import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { Provider } from 'react-redux';
 import React from 'react';
 import { Provider as RollbarProvider } from '@rollbar/react';
-import Rollbar from 'rollbar';
 import App from './components/App';
 import resources from './locales/index.js';
 import store from './app/store';
@@ -11,11 +10,14 @@ import { connectSocket } from './services/socketService.js';
 import { addMessage } from './features/messagesSlice.js';
 import { addChannel, removeChannel, renameChannel } from './features/channelsSlice.js';
 
-console.log(import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN);
-
 const rollbarConfig = {
-  accessToken: '60cb77910f474fdcbcb46d23c87f8d20',
-  environment: import.meta.env.VITE_NODE_ENV || 'production',
+  enabled: process.env.NODE_ENV === 'production',
+  accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
+  addErrorContext: true,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  captureIp: true,
+  environment: 'production',
 };
 
 const init = async () => {
@@ -31,9 +33,6 @@ const init = async () => {
     },
   });
 
-  const rollbar = new Rollbar(rollbarConfig);
-  rollbar.error('Intentional Rollbar Error');
-
   connectSocket(store.dispatch, {
     addMessage,
     addChannel,
@@ -42,7 +41,7 @@ const init = async () => {
   });
 
   return (
-    <RollbarProvider instance={rollbar}>
+    <RollbarProvider config={rollbarConfig}>
       <I18nextProvider i18n={i18n}>
         <Provider store={store}>
           <App />
